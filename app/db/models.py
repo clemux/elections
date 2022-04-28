9c1ef39604339207793192974e2a6cd414829507
Clément Schreiner <clement@mux.me>
2022-04-28 23:41:31 +0200

    add missing models.py

---
+++ app/db/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Float, String
+
+from app.db.database import Base
+
+
+class CircResult(Base):
+    __tablename__ = 'circ_results'
+    code_dept = Column('code_dept', String, primary_key=True)
+    code_circ = Column('code_circ', String, primary_key=True)
+    candidate = Column('candidate', String, primary_key=True)
+    percentage = Column('percentage', Float)
+
+    def __repr__(self):
+        return f"{self.code_dept}{self.code_circ} {self.candidate} {self.percentage}"
